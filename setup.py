70d01d304cbcd81b21265e0e2082834ff41a2d19
Carbon Unit <60440548+qpointConsulting@users.noreply.github.com>
2020-02-06 09:17:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="GoFindMe", # Replace with your own username
+    name="python-GoFindMe", # Replace with your own username
     version="0.0.1",
     author="Bryan Galbraith, Mikah Hoffman",
     author_email="qpoint@shaw.ca",
