d473f99c25045d9b213977d5208324f8446df87f
Carbon Unit <60440548+qpointConsulting@users.noreply.github.com>
2020-02-06 08:50:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="GoFindMe-pkg-qpointconsulting", # Replace with your own username
+    name="GoFindMe", # Replace with your own username
     version="0.0.1",
     author="Bryan Galbraith, Mikah Hoffman",
     author_email="qpoint@shaw.ca",
@@ -12,11 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/qpointconsulting/Go-Find-Me",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where='src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
     ],
     python_requires='>=3.6',
 )
