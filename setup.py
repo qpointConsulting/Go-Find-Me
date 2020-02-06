a758f5c7d76aea389c352057c532bdb3b27f1814
Carbon Unit <60440548+qpointConsulting@users.noreply.github.com>
2020-02-06 08:07:42 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Go-Find-Me-qpointconsulting", # Replace with your own username
+    version="0.0.1",
+    author="Bryan Galbraith, Mikah Hoffman",
+    author_email="qpoint@shaw.ca",
+    description="A small app to locate usernames on various social media sites",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qpointconsulting/Go-Find-Me",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
